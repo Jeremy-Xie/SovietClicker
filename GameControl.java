fc21130c57a8d4a686616df385f9911a7c6b3769
Jeremy-Xie <43050673+Jeremy-Xie@users.noreply.github.com>
2018-09-06 18:52:55 -0400

    Add files via upload

---
+++ GameControl.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GameControl {
+	public int time = 0;
+	public static void main(String[] args) {
+		
+		JFrame F1 = new JFrame("Soviet Clicker");    
+		// TODO Auto-generated method stub
+		JPanel P1 = new JPanel();
+		P1.setBounds(40,80,200,200);    
+		P1.setBackground(Color.gray);  
+		
+		F1.add(P1);
+		F1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		F1.setSize(400,400);    
+        F1.setLayout(null);    
+        F1.setVisible(true); 
+        GameControl object = new GameControl();
+        object.Update();
+	}
+	
+	public synchronized void Update() {
+		while(true) {
+			
+		}
+	}
+}
